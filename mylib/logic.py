f1805742536c8b83bcf9dc1420432b617d62e4a0
Kendall Tenorio <kendall82@hotmail.com>
2026-02-06 00:25:33 -0600

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,9 @@ def wiki(name="War Godddess", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """This is a wikipedia fetcher"""
+
+    my_wiki = wikipedia.summary(name)
+    return my_wiki
